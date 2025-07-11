299a9ae201c9b39c68926a0b7ed084a0a5e8467f
Brandon Tom <brandon@geocaching.com>
2025-07-11 09:27:17 -0700

    chore: remove ResizeObserver polyfill from spinning-box test

--- apps/web/__tests__/spinning-box.test.tsx
+++ apps/web/__tests__/spinning-box.test.tsx
@@ -4,17 +4,6 @@ import { Canvas } from '@react-three/fiber'
 import SpinningBox from '../app/SpinningBox'
 
 test('renders canvas with spinning box', () => {
-  if (!(globalThis as any).ResizeObserver) {
-    const Polyfill = class {
-      observe() {}
-      unobserve() {}
-      disconnect() {}
-    }
-    ;(globalThis as any).ResizeObserver = Polyfill
-    if ((globalThis as any).window) {
-      ;(globalThis as any).window.ResizeObserver = Polyfill
-    }
-  }
   const { container } = render(
     <Canvas>
       <SpinningBox />
