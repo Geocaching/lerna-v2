ec91cb45fdda4436752c309aece050e012432e7e
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-07-10 21:35:49 -0700

    Add Testing Library and jsdom

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg|glb)$': '<rootDir>/__mocks__/fileMock.ts'
