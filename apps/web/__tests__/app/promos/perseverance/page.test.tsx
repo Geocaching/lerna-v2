572664e0bfc06c4b35dc52216d5e0333a33c2b09
Brandon Tom <brandon@geocaching.com>
2025-07-12 08:07:02 -0700

    chore: update PerseverancePromo to use new structure and update test

--- apps/web/__tests__/app/promos/perseverance/page.test.tsx
+++ apps/web/__tests__/app/promos/perseverance/page.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import PerseverancePromo from '@/promos/perseverance/page'
+import { PerseverancePromo } from '@/promos/perseverance/page'
 
 test('renders Hello, world! text', () => {
   render(<PerseverancePromo />)
-  expect(screen.getByText('Hello, world!')).toBeTruthy()
+  expect(screen.getByTestId('rover-explorer')).toBeTruthy()
 })
--- apps/web/app/promos/perseverance/page.tsx
+++ apps/web/app/promos/perseverance/page.tsx
@@ -1,7 +1,19 @@
 export const dynamic = 'force-static'
 
-const PerseverancePromo = () => {
-  return <div>Hello, world!</div>
-}
+export const PerseverancePromo = () => (
+  <div
+    className={
+      'flex flex-col items-center justify-start min-h-screen bg-white w-full'
+    }
+  >
+    <div
+      data-testid={'rover-explorer'}
+      id={'rover-explorer'}
+      className="flex justify-center bg-[url('https://gs-strapi.s3.us-east-1.amazonaws.com/Topographic_Pattern_Tiling_op35_d47c7e78ef.png')] bg-size-[400px] w-full max-w-[85vw] sm:max-w-full"
+    >
+      Hello, world!
+    </div>
+  </div>
+)
 
 export default PerseverancePromo
