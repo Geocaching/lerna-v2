f68e1bb086c9d902330c5188d0da46562d927897
Brandon Tom <brandon@geocaching.com>
2025-07-12 14:19:39 -0700

    chore: remove unused background image from PerseverancePromo

--- apps/web/app/promos/perseverance/page.tsx
+++ apps/web/app/promos/perseverance/page.tsx
@@ -9,7 +9,7 @@ export const PerseverancePromo = () => (
     <div
       data-testid={'rover-explorer'}
       id={'rover-explorer'}
-      className="flex justify-center bg-[url('https://gs-strapi.s3.us-east-1.amazonaws.com/Topographic_Pattern_Tiling_op35_d47c7e78ef.png')] bg-size-[400px] w-full max-w-[85vw] sm:max-w-full"
+      className='flex justify-center bg-size-[400px] w-full max-w-full'
     >
       Hello, world!
     </div>
