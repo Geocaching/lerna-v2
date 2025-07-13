12254d4d3d6c98cd6d09c0fdc0b9bf769c59c564
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-07-13 14:15:10 -0700

    refactor: reorganize glb-model src

--- packages/glb-model/rollup.config.js
+++ packages/glb-model/rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/index.esm.js',
