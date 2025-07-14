814be7431a494f64993161634c6a6cb90a8b940f
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-07-13 19:16:53 -0700

    chore: consolidate jest config

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }]
   },
   moduleNameMapper: {
+    '^@/app/(.*)$': '<rootDir>/apps/web/app/$1',
     '^@/(.*)$': '<rootDir>/apps/web/$1',
     '^@geocaching/glb-model$': '<rootDir>/packages/glb-model/src'
   }
